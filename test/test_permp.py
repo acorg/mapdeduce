ab9bde8d7cfc373e80911f87ca7c33d5ae582b2a
David Pattinson <djp65@cam.ac.uk>
2017-10-25 15:16:48 +0100

    Wrapper to R function for computing empirical p-values

---
+++ test/test_permp.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""Tests for permp function that computes empirical p-values"""
+
+from MapSeq.permp import permp
+import numpy as np
+import unittest2 as unittest
+from rpy2.rinterface import RRuntimeError
+
+
+class Permp(unittest.TestCase):
+    """Tests for permp function"""
+
+    def test_expected_result1_(self):
+        """Compare python result to R result"""
+        p = permp(1, 1000, 3, 997)[0]
+        self.assertTrue(np.abs(p - 0.001997999) < 1e-10)
+
+    def test_too_many_combinations_for_exact(self):
+        """When there are too many permutations, expec an RRuntimeError"""
+        with self.assertRaises(RRuntimeError):
+            permp(1, 1000, 500, 500, method="exact")
+
+    def test_approximate_method(self):
+        """
+        When there are too many permutations for exact, approximate shold not
+        raise an error.
+        """
+        permp(1, 1000, 500, 500, method="approximate")
+
+    def test_returns_np_array(self):
+        """Should return an np array"""
+        p = permp(1, 1000, 3, 997)
+        self.assertIsInstance(p, np.ndarray)

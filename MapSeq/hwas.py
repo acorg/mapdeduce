18ae3155b23b25e90acca4da36acc447218a5139
David Pattinson <djp65@cam.ac.uk>
2017-11-04 15:26:33 +0000

    Simplified the HWAS code

--- MapSeq/hwas.py
+++ MapSeq/hwas.py
@@ -3,52 +3,16 @@
 import numpy as np
 import scipy
 import sklearn
-from limix.qtl import qtl_test_lmm
+
+from limix.qtl import qtl_test_lmm, qtl_test_lmm_kronecker
+from limix.vardec import VarianceDecomposition
+
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from permp import permp
 from warnings import warn
-
-
-def pd_qtl_test_lmm(**kwargs):
-    """
-    Pandas wrapper for qtl_test_lmm. qtl_test_lmm "snps" and "pheno" kwargs
-    now have to be pd.DataFrame intances. Checks that the indexes for both
-    those dfs are the same. Attaches the p-values, effect sizes and standard
-    errors as a pd.Panel to the lmm object.
-
-    @param kwargs: Passed to qtl_test_lmm
-    """
-    snps = kwargs.pop("snps", None)
-    pheno = kwargs.pop("pheno", None)
-    n_tests = kwargs.pop("n_tests", 1)
-    assert (snps.index != snps.index).sum() == 0
-    lmm = qtl_test_lmm(snps.values, pheno.values, **kwargs)
-
-    lmm.n_tests = n_tests
-    corrected = lmm.getPv() * n_tests
-    corrected[corrected > 1] = 1  # No p value should exceed 1
-
-    lmm.results = pd.Panel(
-        data=[
-            lmm.getPv(),
-            lmm.getBetaSNP(),
-            lmm.getBetaSNPste(),
-            corrected,
-        ],
-        items=[
-            "p-values",
-            "effect-size",
-            "std-error",
-            "p-values-corrected",
-        ],
-        major_axis=pheno.columns,
-        minor_axis=snps.columns,
-    )
-
-    return lmm
 
 
 def shuffle_values(nperm, values):
@@ -99,38 +63,37 @@ def effective_tests(snps):
     return (np.sum(np.sqrt(eigenvalues)) ** 2) / np.sum(eigenvalues)
 
 
-def qq_plot(results, effect_size=True, snps=None, **kwargs):
+def qq_plot(results, snps=None, **kwargs):
     """
     Plot a quantile-quantile comparison plot of p-values
 
-    @param results pd.Panel: Like pd.Panel returned by pd_qtl_test_lmm.
-        items: p-values, effect-size, std-error
-        major axis: Phenotypes
-        minor axis: SNPs
-    @param effect_size: Bool. Plot the effect size with error bars
+    @param results pd.DataFrame: Like pd.Panel returned by pd_qtl_test_lmm.
+        columns must contain "p" and can also contain the following:
+                    p-corrected
+                    beta
+                    std-error
+                    p-empirical
+        DataFrame indexes are SNPs.
     @param snps: List. Plot only these snps
 
         Optional kwargs
 
-    @param phenotype: Specify the phenotype to plot. Default is the first
-        phenotype in self.pheno.columns
     @param larger: List. SNPs to plot larger.
     @param very_large: List. SNPs to plot very large.
     """
     ax = plt.gca()
-    phenotype = kwargs.pop("phenotype", results.major_axis[0])
     larger = kwargs.pop("larger", None)
     very_large = kwargs.pop("very_large", None)
 
     # Get 2D DataFrame contianing pvalues and effect sizes for this
     # phenotype
-    df = results[:, phenotype, :]
+    df = results
     if snps is not None:
         print "Only plotting substitutions at these positions:\n{}".format(
             ",".join(map(str, snps))
         )
         df = pd.concat(map(lambda x: df.filter(regex=str(x), axis=0), snps))
-    df.sort_values("p-values", inplace=True)
+    df.sort_values("p", inplace=True)
 
     if larger is not None:
         s = pd.Series(np.array([i in larger for i in df.index]) * 125 + 25,
@@ -142,43 +105,50 @@ def qq_plot(results, effect_size=True, snps=None, **kwargs):
         for vl in very_large:
             s[vl] = 325
 
-    # Parameters for the qq plot
+    # qq plot parameters
     n = df.shape[0]
-    x = pd.Series(-1 * np.log10(np.linspace(1 / float(n), 1, n)),
-                  index=df.index)
+    x = pd.Series(
+        -1 * np.log10(np.linspace(1 / float(n), 1, n)),
+        index=df.index
+    )
     scatter_kwds = dict(x=x, edgecolor="white", s=s)
-    ax.scatter(y=-1 * np.log10(df["p-values"]),
+
+    ax.scatter(y=df["logp"],
                zorder=20,
                label="-log10(p-value)",
                c="#c7eae5",
                **scatter_kwds)
-    ax.scatter(y=-1 * np.log10(df["p-values-corrected"]),
-               zorder=20,
-               label="-log10(Corrected p-value)",
-               c="#35978f",
-               **scatter_kwds)
-    if "p-empirical" in df.columns:
-        ax.scatter(y=-1 * np.log10(df["p-empirical"]),
+
+    try:
+        ax.scatter(y=df["logp-corrected"],
+                   zorder=20,
+                   label="-log10(Corrected p-value)",
+                   c="#35978f",
+                   **scatter_kwds)
+    except KeyError:
+        pass
+
+    try:
+        ax.scatter(y=-1 * np.log10(df["logp-empirical"]),
                    zorder=18,
                    label="-log10(Empirical p-value",
                    c="#003c30",
                    **scatter_kwds)
-    ax.scatter(y=np.abs(df.loc[:, "effect-size"]),
-               label="Abs. effect size",
-               c="#a6611a",
-               zorder=15,
-               **scatter_kwds)
-    ax.errorbar(x=x,
-                y=np.abs(df.loc[:, "effect-size"]),
-                yerr=df.loc[:, "std-error"],
-                lw=0,
-                elinewidth=1,
-                ecolor="grey",
-                label="Effect size std. err",
-                zorder=10)
+    except KeyError:
+        pass
+
+    try:
+        ax.scatter(y=df.loc[:, "joint-effect"],
+                   label="Joint-effect",
+                   c="#a6611a",
+                   zorder=15,
+                   **scatter_kwds)
+    except KeyError:
+        pass
+
     # Label larger SNPs
     if larger is not None:
-        y = -1 * np.log10(df["p-values-corrected"])
+        y = df["logp"]
         for snp in larger:
             ax.text(x=x[snp],
                     y=y[snp] + 0.05,
@@ -188,11 +158,13 @@ def qq_plot(results, effect_size=True, snps=None, **kwargs):
                     zorder=20,
                     fontsize=10,
                     rotation=90)
+
     ax.set_xlabel(r"Null $-log_{10}$(p-value)")
     xmax, ymax = ax.get_xlim()[1], ax.get_ylim()[1]
     ax.set_xlim(0, xmax)
     ax.set_ylim(0, ymax)
     ax.plot((0, 50), (0, 50), c="white", zorder=10)  # x = y
+
     return ax
 
 
@@ -214,8 +186,10 @@ class HwasLmm(object):
         self.P = pheno.shape[1]  # n phenotypes
         self.snps = snps
         self.pheno = pheno
-        self.K = cov(snps)  # Covariance matrix of snps
-        self.n_tests = effective_tests(snps)
+        self.K = cov(snps)
+        self.n_tests = 1 if self.S == 1 else effective_tests(self.snps)
+        if self.P > 1:
+            self.Asnps = np.eye(self.P)
 
     def compute_k_leave_each_snp_out(self):
         """Leave each snp out of self.snps and compute a covariance matrix.
@@ -226,41 +200,55 @@ class HwasLmm(object):
             s: cov(self.snps.drop(s, axis=1)) for s in self.snps.columns
         }
 
-    def lmm(self, K_without_snp=True, **kwargs):
-        """Run lmm on the snps.
+    def lmm(self):
+        """Run lmm on the snps."""
+        if not hasattr(self, "K_leave_out"):
+            self.compute_k_leave_each_snp_out()
 
-        @param K_without_snp. Bool. For each snp, use a covariance matrix
-            computed with that snp ommitted.
+        results = {}
 
-            Optional kwargs:
+        for snp in tqdm(self.snps.columns):
 
-        @param snps. df (N, S). N individuals, S snps
-        """
-        snps = kwargs.pop("snps", self.snps)
+            if self.P == 1:
 
-        if K_without_snp:
-            results = {}
+                lmm = qtl_test_lmm(
+                    snps=self.snps.loc[:, [snp, ]].values,
+                    pheno=self.pheno.values,
+                    K=self.K_leave_out[snp]
+                )
 
-            if not hasattr(self, "K_leave_out"):
-                self.compute_k_leave_each_snp_out()
+                beta = lmm.getBetaSNP()[0, 0]
 
-            # Compute the number of tests
-            n_tests = 1 if self.S == 1 else float(effective_tests(snps))
+            else:
 
-            for snp in tqdm(snps.columns):
-                lmm = pd_qtl_test_lmm(snps=snps.loc[:, [snp, ]],
-                                      pheno=self.pheno,
-                                      K=self.K_leave_out[snp],
-                                      n_tests=n_tests)
-                results[snp] = lmm.results[:, :, snp]
-            results = pd.Panel.from_dict(results, orient="minor")
+                lmm, pv = qtl_test_lmm_kronecker(
+                    snps=self.snps.loc[:, [snp, ]].values,
+                    phenos=self.pheno.values,
+                    Asnps=self.Asnps,
+                    K1r=self.K_leave_out[snp]
+                )
 
-        else:
-            warn("SNP being tested included in kinship matrix.")
-            lmm = pd_qtl_test_lmm(snps=snps, pheno=self.pheno, K=self.K)
-            results = lmm.results
+                beta = lmm.getBetaSNP()[:, 0]
 
-        return results
+            results[snp] = {
+                "p": lmm.getPv()[0, 0],
+                "beta": beta,
+            }
+
+        df = pd.DataFrame.from_dict(results, orient="index")
+        df.sort_values("p", inplace=True)
+
+        corrected = df["p"] * self.n_tests
+        corrected[corrected > 1] = 1
+        df["p-corrected"] = corrected
+
+        df["logp"] = np.log10(df["p"]) * -1
+        df["logp-corrected"] = np.log10(df["p-corrected"]) * -1
+
+        if self.P > 1:
+            df["joint-effect"] = df["beta"].apply(np.linalg.norm)
+
+        return df
 
     def lmm_permute(self, n, K_without_snp=False, **kwargs):
         """Run lmm on n shuffled permutations of snps.
@@ -282,7 +270,7 @@ class HwasLmm(object):
         for i in range(n):
             results = self.lmm(snps=sklearn.utils.shuffle(snps),
                                K_without_snp=K_without_snp)
-            pvalues[i, :] = results.loc["p-values", :, :]
+            pvalues[i, :] = results.loc["p", :, :]
 
         df = pd.DataFrame(pvalues)
         df.columns = snps.columns
@@ -303,12 +291,13 @@ class HwasLmm(object):
             ser = results.loc["p-empirical", pheno, :]
             print ser[ser.notnull()]
 
-        pvalues = results.loc["p-values-corrected", pheno, :]
+        pvalues = results.loc["p-corrected", pheno, :]
         snps_below_cutoff = pvalues.index[pvalues < cutoff]
         empirical_pvalues = {}
         for snp in tqdm(snps_below_cutoff):
             arr = shuffle_values(nperm=nperm,
                                  values=self.snps.loc[:, snp].values)
+
             lmm = qtl_test_lmm(snps=arr,
                                pheno=self.pheno.values,
                                K=self.K_leave_out[snp])

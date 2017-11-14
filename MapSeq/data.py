375bec21609a92c115a1819c0504aad0d3e86620
David Pattinson <djp65@cam.ac.uk>
2017-11-14 18:31:56 +0000

    Added cluster difference AAPs

--- MapSeq/data.py
+++ MapSeq/data.py
@@ -25,3 +25,50 @@ amino_acids = {
 
 not_109_to_301 = range(1, 109) + range(302, 329)
 not_110_to_199 = range(1, 110) + range(200, 329)
+
+
+# Cluster difference amino acid polymorphisms for post SY97
+# Doesn't include positions outside of 109-301
+
+clus_diff_aaps = {
+    "SY97-FU02": {
+        "131A",
+        "131T",
+        "144D",
+        "144N",
+        "155H",
+        "155T",
+        "156Q",
+        "156H",
+        "202V|222W",
+        "202I|222R",
+        "225G",
+        "225D",
+    },
+    "FU02-CA04": {
+        "145K",
+        "145N",
+        "189S",
+        "189N",
+        "226V",
+        "226I",
+        "159F",
+        "159Y",
+        "227S",
+        "227P",
+    },
+    "CA04-WI05": {
+        "225D",
+        "225N",
+        "193F",
+        "193S",
+    },
+    "WI05-PE09": {
+        "189N",
+        "189K",
+        "144N",
+        "144K",
+        "158K",
+        "158N",
+    }
+}

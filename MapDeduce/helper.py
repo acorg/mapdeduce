a7aef0ac1328962a67845bec18d2df8027cf2ef1
David Pattinson <djp65@cam.ac.uk>
2018-08-22 14:33:39 +0100

    Added helper function to expand sequences to a DataFrame.

--- MapDeduce/helper.py
+++ MapDeduce/helper.py
@@ -1,5 +1,6 @@
 """Helper functions."""
 
+import pandas as pd
 from data import amino_acids
 
 
@@ -21,3 +22,38 @@ def is_not_amino_acid(a):
         return True
     else:
         return False
+
+def string_to_series(arg):
+    """Each element in string becomes element in series
+
+    Notes:
+        If arg is not cannot be coerced to a series, return an empty series.
+
+    Args:
+        arg (str)
+
+    Returns:
+        pd.Series
+    """
+    try:
+        return pd.Series(tuple(arg))
+    except TypeError:
+        return pd.Series()
+
+def expand_sequences(series):
+    """Expand Series containing sequences into DataFrame.
+
+    Notes:
+        Any elements in series that cannot be expanded will be dropped.
+
+    Args:
+        series (pd.Series)
+
+    Returns:
+        pd.DataFrame: Columns are sequence positions, indexes match
+            series.index.
+    """
+    df = series.apply(string_to_series)
+    df.columns = range(df.shape[1])
+    df.columns += 1
+    return df[df.notnull().all(axis=1)]
---
+++ test/test_helper.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+"""Tests for helper functions."""
+
+import numpy as np
+import pandas as pd
+from MapDeduce.helper import expand_sequences
+import unittest2 as unittest
+
+
+class ExpandSequences(unittest.TestCase):
+    """Tests for MapDeduce.helper.expand_sequences"""
+
+    def test_returns_df(self):
+        series = pd.Series(['abc', 'def'])
+        df = expand_sequences(series)
+        self.assertIsInstance(df, pd.DataFrame)
+
+    def test_handles_nan(self):
+        series = pd.Series(['abc', np.nan, 'def'])
+        expand_sequences(series)
+
+    def test_columns_are_integers(self):
+        series = pd.Series(['abc', np.nan, 'def'])
+        df = expand_sequences(series)
+        self.assertEqual(1, df.columns[0])
+
+
+if __name__ == "__main__":
+    unittest.main()

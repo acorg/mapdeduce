1aa23cd1696ce1c54803b6285a997f80a211af7b
David Pattinson <djp65@cam.ac.uk>
2019-05-29 13:42:25 -0400

    Simplified importing core classes.

--- mapdeduce/__init__.py
+++ mapdeduce/__init__.py
@@ -1,1 +1,13 @@
-__version__ = '1.0.0'
+from .mapseq import MapSeq, OrderedMapSeq
+from .hwas import HwasLmm
+from .dataframes import CoordDf, SeqDf
+
+__version__ = "1.0.0"
+
+__all__ = [
+    MapSeq,
+    OrderedMapSeq,
+    HwasLmm,
+    CoordDf,
+    SeqDf
+]

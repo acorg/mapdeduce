4cc824d333911f52aacb7a8e83f62d1bf66ea8ec
David Pattinson <david.pattinson@wisc.edu>
2026-01-20 13:43:09 -0500

    Addded some objects to the mapdeduce module namespace.

---
+++ mapdeduce/__init__.py
@@ -0,0 +1,6 @@
+from .mapseq import OrderedMapSeq
+from .blup import LmmBlup
+from .hwas import cov
+from .plotting import make_ax_a_map
+
+__all__ = ["cov", "LmmBlup", "OrderedMapSeq", "make_ax_a_map"]

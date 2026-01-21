acc0db5c0b56c2802a0897e56e46a5ac76b65eb2
David Pattinson <david.pattinson@wisc.edu>
2026-01-21 10:59:40 -0500

    Added CoordDf and SeqDf to main mapdeduce namespace.

--- mapdeduce/__init__.py
+++ mapdeduce/__init__.py
@@ -1,6 +1,14 @@
 from .blup import LmmBlup
+from .dataframes import CoordDf, SeqDf
 from .hwas import cov
 from .mapseq import OrderedMapSeq
 from .plotting import make_ax_a_map
 
-__all__ = ["cov", "LmmBlup", "OrderedMapSeq", "make_ax_a_map"]
+__all__ = [
+    "CoordDf",
+    "cov",
+    "LmmBlup",
+    "make_ax_a_map",
+    "OrderedMapSeq",
+    "SeqDf",
+]
